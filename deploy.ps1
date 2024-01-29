b14ce06190cab0469b1f7549e1d51ddc2d9142cc
Hazem Safwat <HSafwat@moj.gov.sa>
2024-01-29 05:53:49 +0300

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -3,4 +3,8 @@
 Write-Host "Building pink_web app for staging environment..."
 git commit -am "deploy"
 git push
-ssh pinkstaging2 "node -v && cd /var/www/node-bots/chatbot_api && git reset --hard HEAD && git pull && npm install && npm run build  && truncate -s 0 /home/ubuntu/.pm2/logs/chatbot-api1-out.log  && /home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 restart chatbot_api1  && echo "$(Get-date) - node-bots/stg" >> log.log"
+ssh pinkstaging2 "node -v && cd /var/www/node-bots/chatbot_api \
+&& git reset --hard HEAD && git pull && npm install && npm run build  \
+&& truncate -s 0 /home/ubuntu/.pm2/logs/chatbot-api1-out.log  \
+&& /home/ubuntu/.nvm/versions/node/v18.17.0/bin/pm2 restart chatbot_api1  \
+&& echo "$(Get-date) - node-bots/stg" >> log.log"
