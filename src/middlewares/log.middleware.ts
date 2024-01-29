6b4460c525ef0b1f27332457f3155955e74fe0cf
Hazem Safwat <HSafwat@moj.gov.sa>
2024-01-29 06:33:16 +0300

    deploy

--- src/middlewares/log.middleware.ts
+++ src/middlewares/log.middleware.ts
@@ -9,7 +9,7 @@ export class LogMiddleware implements NestMiddleware {
 
   async use(req: Request, res: Response, next: () => void) {
     console.log('LogMiddleware_req.body', req.url, req.body);
-    console.log('LogMiddleware_res', res.body);
+    console.log('LogMiddleware_res', res);
     next();
   }
 }
