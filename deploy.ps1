d485e4f8827730ad5919b6adb7313f85a3e3dde5
Hazem Safwat <HSafwat@moj.gov.sa>
2024-01-29 05:26:01 +0300

    deploy

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,6 @@
 
 # Commands for building the provider_web app in staging environment
 Write-Host "Building pink_web app for staging environment..."
+git commit -am "deploy"
+git push
 ssh pinkstaging2 "node -v && cd /var/www/node-bots/chatbot_api && git pull && npm install && npm run build && pm2 restart chatbot_api1 && truncate -s 0 /home/ubuntu/.pm2/logs/chatbot-api1-out.log  && echo "$(Get-date) - node-bots/stg" >> log.log"
